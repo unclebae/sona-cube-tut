dd6c7895a60b1af94814fbdb0bc14e170513d404
SKT.배기도 <skt.1111489@sk.com>
2020-03-23 16:13:12 +0900

    add sona cube

---
+++ src/main/java/com/eunkong/eschool/sonacubetest/SonaCubeTestApplication.java
@@ -0,0 +1,13 @@
+package com.eunkong.eschool.sonacubetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonaCubeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonaCubeTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eunkong/eschool/sonacubetest/SonaCubeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eunkong.eschool.sonacubetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonaCubeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
